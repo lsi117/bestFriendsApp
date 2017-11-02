2232bbb7bc6bb7fb0dfce7d2a4b94bae8a98ac7f
lauren inacio <lauren_f23@yahoo.com>
2017-11-02 11:27:19 -0400

    finished styling

--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -1,4 +1,4 @@
---\c bf readd connection for local deployment
+\c bf readd connection for local deployment
 INSERT INTO shelters (name, zipcode, description, pets) VALUES
 ('JustDogs Shelter', '10002', 'a lovely shelter that has just dogs up for adoption', 'dogs'),
 ('Kittens and Cats Shelter', '10009', 'located in the east village a shelter for cats', 'cats'),
